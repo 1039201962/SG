f2eb369dabb41e59f384fd60bb68e4117451e884
Administrator <Administrator@176.201.107.13>
2019-10-22 17:03:04 +0800

    补全目录

---
+++ sgshop/sg-web/src/main/java/com/sg/SpringBootRun.java
@@ -0,0 +1,17 @@
+package com.sg;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+@MapperScan("com.sg.mapper")
+public class SpringBootRun {
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(SpringBootRun.class, args);
+	}
+}
